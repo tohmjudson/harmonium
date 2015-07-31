a05969df084b685c892d3129167f310ca59f5286
Tohm Judson <tohm@tohmjudson.com>
2015-07-31 13:25:24 -0600

    better functionality, cleaned code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,23 +1,52 @@
 $(function() {
 
+    var socket = io();
+
+//=============== USER =====================//
+    var username = prompt('What\'s your username?');// The visitor is asked for their username...
+    socket.emit('newUser', username); // It's sent with the signal "newUser"
+    socket.on('message', function (message) { // A log is displayed when the server sends us a "message"
+        console.log('SERVER: ' + message);
+    })
+
 //=============== POKE FOR TESTING CONNECTION ====================//
-    $('#poke').click(function () {
-        socket.emit('message', 'Hi server, how are you?');
-    })
+        $('#poke').click(function () {
+            socket.emit('message', 'Hi server, how are you?');
+        })
 
-// Key 1 
-    $("#key1").on("mousedown", function() {
-        //socket.emit('message', 'Pressed Key 1');
-    })
+//=============== AUDIO =====================//
+    var playKey = function (data) {
+      $('#' + data.id);// Passes button id
+    }
 
-    $("#key1").on("mouseup", function() {
-    })
+//=============== EMIT = SUBMIT =====================//
+    $('.key').click(function () {
+      var keyData = {
+        id: $(this).attr('id')
+      }
+      //playKey(keyData);
+      socket.emit('keyPress', keyData);
+    });
 
-// Key 2
-    $("#key2").on("mousedown", function() {
-        //socket.emit('message', 'Pressed Key 2');
-    })
+//=============== ON = LISTEN =====================//
+    socket.on('keyPressed', function(data){
+      playKey(data);
+      console.log(data);
 
-    $("#key2").on("mouseup", function() {
-    })
-});
+    switch (data['id']) {
+        case '0':
+            playButton01();
+            break;
+        case '1':
+            playButton02();
+            break;
+        case '2':
+            playButton03();
+            break;
+        case '3':
+            playButton04();
+            break;
+        };
+    });
+
+}); //Wrap
